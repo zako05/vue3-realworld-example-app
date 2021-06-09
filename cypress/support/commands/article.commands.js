d7adf132c224f75b3c3dd47505fdf819886ead0d
Michal Zak <zako05@gmail.com>
2021-06-09 12:53:21 +0200

    add article commands

---
+++ cypress/support/commands/article.commands.js
@@ -0,0 +1,26 @@
+Cypress.Commands.add('createArticle', (token, title, about, body) => {
+  Cypress.log({
+    name: 'createArticle',
+    message: `${token} ${title} | ${about} | ${body}`
+  })
+
+  const apiArticles = `${Cypress.env('apiUrl')}/articles`
+
+  return cy.request({
+    method: 'POST',
+    url: `${apiArticles}`,
+    headers: {
+      authorization: `Token ${token}`
+    },
+    body: {
+      article: {
+        title: title,
+        description: about,
+        body: body,
+      }
+    }
+  }).then(response => {
+    expect(response.status).to.eq(200)
+    // return response.article.slug
+  })
+})
